aa3a2fd4d146f556970cea744b7ad06e9133c780
Jay <Jay@Jays-MacBook-Pro.local>
2022-09-24 17:51:16 -0700

    Input and button properly functions. Created async axios api call using hooks that successfully makes a request to Jikan API

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,35 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
     const [id, setId] = useState("");
+    const [char, setChar] = useState([]);
+
     const handleInput = () => {
         console.log(id);
     };
 
+    useEffect(() => {
+        (async () => {
+            const char = await axios.get(
+                "https://api.jikan.moe/v4/characters/2727"
+            );
+            console.log(char.data.data)
+            setChar(char.data.results);
+        })();
+    }, []);
+
+
+// ~150k+ characters
     return(
         <div>
-            <input placeholder = "char ID" onChange = {e => setId = (e.target.value)}/>
+            <input
+                type = "number"
+                min = "0"
+                onKeyDown={(e) => (e.key === 'e' || e.key === '-' || e.key === "+" || e.key === ".") && e.preventDefault() }
+                placeholder = "char ID"
+                onChange = {e => setId(e.target.value)}
+            />
             <button onClick = {() => handleInput()}>Add</button>
         </div>
     )
